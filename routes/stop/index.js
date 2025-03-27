f8e91f1e7ce76a28155397e30119b881c0305116
Bruno Ilponse <bruno.ilponse@isae-supaero.fr>
2025-03-27 08:55:12 +0100

    test

--- routes/stop/index.js
+++ routes/stop/index.js
@@ -4,6 +4,6 @@ const state = require('../../state')
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     state.started = false;
-    return `Stopped`
+    return `Stopped.`
   })
 }
